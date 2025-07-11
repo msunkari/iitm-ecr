1a2677cd9c81dd2af57bc2e6458e5a4940ef68ac
Sandeep <settisandeep@gmail.com>
2025-07-11 12:08:32 +0530

    Updates

---
+++ _astro/hoisted.D3hvUd5G.js
@@ -0,0 +1,1 @@
+import"https://cdn.jsdelivr.net/npm/glider-js@1.7.8/glider.min.js";document.addEventListener("DOMContentLoaded",function(){new Glider(document.querySelector(".glider"),{slidesToShow:1,dots:".dots",draggable:!0,scrollLock:!0,rewind:!0,arrows:{prev:".glider-prev",next:".glider-next"}})});
